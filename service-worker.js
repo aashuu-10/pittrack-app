abdff8f5375cc407dd2f0c28708c0813cdfe1b33
aashuu-10 <aaayushkumar22@gmail.com>
2026-08-29 23:49:28 +0530

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "bcclAR-v6";
+const CACHE_NAME = "bcclAR-v7";
 const ASSETS = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png", "./logo.png"];
 
 self.addEventListener("install", (event) => {
